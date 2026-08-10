16bbd9a517aac02d917c96d7b44d7738942cd8b5
Zylaah <51694937+Zylaah@users.noreply.github.com>
2026-08-10 15:46:38 +0200

    Layer artwork dim on card; bump to 1.4.1
    Replace the ::before overlay with a layered background gradient on the media card to apply a dim over artwork without z-index conflicts. Remove the pseudo-element rules and simplify stacking. Also bump the addon/userscript version from 1.4.0 to 1.4.1 in media-volume-slider.uc.js and theme.json.

--- media-volume-slider/media-volume-slider.uc.js
+++ media-volume-slider/media-volume-slider.uc.js
@@ -2,7 +2,7 @@
 // @name           Better Media Toolbar
 // @description    Volume slider on mute hover and media artwork background on toolbar hover
 // @author         Zylaah
-// @version        1.4.0
+// @version        1.4.1
 // @namespace      https://github.com/Zylaah/zen-mods
 // ==/UserScript==
 
